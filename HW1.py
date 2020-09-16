70fa73c11ace778af690ef9e33edbf806bfedf8a
Pasindu Tennakoon <tennakoonpasindu@gmail.com>
2020-09-15 20:47:16 -0500

    Bug and comment fix

--- HW1.py
+++ HW1.py
@@ -1,7 +1,7 @@
 
 # Reads RNA in FASTA format from a file
-# Parameter filename: A string containing the relative path of the file from where
-# this program is stored
+# Parameter filename: A string containing the relative path of the file from
+# where this program is stored
 # Return: The RNA sequences contained in the file in a dictionary format
 def readFile(filename):
     # Open the file and initialize the dictionary
@@ -10,53 +10,54 @@ def readFile(filename):
     for line in file:
         # Remove the newline characters and spaces at the end
         line = line.strip()
-        
+
         # If the line starts with the '>' character
         # then we can start reading the RNA sequence that starts
         # from the next line
         if line.startswith('>'):
-    
+
             # The FASTA format allows spaces, numbers and newlines
             # between the RNA sequences
-            
+
             # List to hold the gene lines
             gene = []
-            
+
             # Read the first line of the RNA string
             gene_line = file.readline()
-            
+
             # While the gene_line is not empty
             while (gene_line):
                 # Clean the line
                 # - Remove the numbers
                 for i in range(10):
-                    gene_line.replace(str(i), '')
-                    
+                    gene_line = gene_line.replace(str(i), '')
+
                 # - Remove whitespace
-                gene_line.replace(' ', '')
-                gene_line.replace('\n', '')
-                
-                # If the resulting line is not empty, add it to 
+                gene_line = gene_line.replace(' ', '')
+                gene_line = gene_line.replace('\n', '')
+
+                # If the resulting line is not empty, add it to
                 # the list of gene_lines
                 if gene_line:
                     gene.append(gene_line)
-                    
+
                 # Read the next line of RNA string
                 gene_line = (file.readline()).strip()
-                
+
             # Add a new gene to the dictionary where
-            # Key:   The line that starts with '>' 
+            # Key:   The line that starts with '>'
             #        ('>' character removed)
             # Value: The RNA sequence of the gene
             genes[line[1:]] = ''.join(gene)
-            
+
     # Close the file and return the dictionary object
     file.close()
     return genes
 
-# This a lookup table that converts 3 character amino acids to
-# single character IUPAC codes 
-triple_letter_to_single_letter = {   
+
+# This a lookup table that converts 3 character amino acids codes to
+# single character IUPAC codes
+triple_letter_to_single_letter = {
     'Ala': 'A',
     'Arg': 'R',
     'Asn': 'N',
@@ -79,12 +80,14 @@ triple_letter_to_single_letter = {
     'Trp': 'W',
     'Tyr': 'Y',
     'Val': 'V',
-    '*'  : '*'
+    '*': '*'
 }
 
 # Sets up a dictionary that matches 3 RNA nucleotides to their corresponding
 # amino acid or to Stop (denoted by '*')
-# Return: The dictionary object that converts 3 nucleotide code to protein
+# Return: The dictionary object that converts 3 nucleotides to amino acids
+
+
 def setUpTable():
     table = {}
     for key in ["UUU", "UUC"]:
@@ -131,51 +134,57 @@ def setUpTable():
         table[key] = "*"
     return table
 
-# Converts an RNA sequence into the corresponding amino acid sequence. Does not
-# yet handle stop codons besides adding stop
+# Converts an RNA sequence into the corresponding amino acid sequence.
+# Stop codones are denoted by '*'
 # Parameter RNA: The RNA sequence in string format
 # Parameter table: The dictionary that pairs RNA to amino acids
-# Parameter single_letter: if this value is True AA is in single character IUPAC codes 
-#                          if this value is False AA is in 3 character codes
+# Parameter single_letter: AA are in single character IUPAC codes (True)
+#                          AA are in 3 character codes (False)
 # Return: A list containing the amino acid sequence
-def convertToAAs(RNA, table, single_letter = False):
-    # Initialize the sequence to an empyt list 
+
+
+def convertToAAs(RNA, table, single_letter=False):
+    # Initialize the sequence to an empty list
     # and the index pointer to 0
-    
+
     seq = []
     current = 0
-    
+
     # Loop over the RNA string length in multiples of 3
     while current + 2 < len(RNA):
         # Convert the RNA condones to amino acids using the
         # look up table
         if single_letter:
-            seq.append(triple_letter_to_single_letter[table[RNA[current:current + 3]]])
+            seq.append(
+                triple_letter_to_single_letter[table[RNA[current:current + 3]]])
         else:
             seq.append(table[RNA[current:current + 3]])
-            
+
         current += 3
-    
+
     return seq
 
 # This is the main function of our translation program
+
+
 def main():
     # Read the file into a dictionary of genes
     genes = readFile("Assignment1Sequences.txt")
-    
+
     # Initialize the lookup table
     AATable = setUpTable()
-    
+
     # Holds the resulting genes in AA format
     AASeqs = {}
     for gene in genes:
-        # Convert RNA string to AA format and store in the 
+        # Convert RNA string to AA format and store in the
         # AASeqs dictionary
-        AASeqs[gene] = (convertToAAs(genes[gene], AATable, single_letter = True))
-    
+        AASeqs[gene] = (convertToAAs(genes[gene], AATable, single_letter=True))
+
     # This part is for displaying the genes and their AA sequences
     for gene in AASeqs:
         print('{}:\n{}\n'.format(gene, ''.join(AASeqs[gene])))
-    
+
+
 if __name__ == '__main__':
     main()
