1852d51c8490a6cea998dffbf14038d8c7a889ed
Pasindu Tennakoon <tennakoonpasindu@gmail.com>
2020-11-30 15:30:21 -0600

    Optimized dp_distance

--- Final/dp_distance/distance.cpp
+++ Final/dp_distance/distance.cpp
@@ -14,29 +14,39 @@ static PyObject * dp_distance(PyObject * self, PyObject * args) {
 
     int n = strlen(s1);
     int m = strlen(s2);
-
     
-    int *table = new int[(n+1) * (m+1)];
-    table[0] = 0;
 
-    for (int i = 1; i < n+1; ++i) table[i * (m+1)] = table[(i-1) * (m+1)] + 1;
-    for (int i = 1; i < m+1; ++i) table[i] = table[i-1] + 1;
+    int *old = new int[(m+1)];
+    int *later = new int[(m+1)];
+
+    // cout << "Got here" << endl;
+    
+    old[0] = 0;
+    for (int i = 1; i < m+1; ++i) old[i] = old[i-1] + 1;
+
+
+    // cout << "and here" << endl;
 
     for (int i = 1; i < n+1; ++i) {
+        later[0] = i;
         for (int j = 1; j < m+1; ++j) {
             if (s1[i-1] == s2[j-1]) {
-                table[(i * (m+1)) + j] = table[((i-1) * (m+1)) + (j-1)];
+                later[j] = old[j-1];
             }
             else {
-                table[(i * (m+1)) + j] = min(table[((i-1) * (m+1)) + (j-1)] + 1, 
-                    min(table[((i) * (m+1)) + (j-1)] + 1, 
-                        table[((i-1) * (m+1)) + j]+ 1));
+                later[j] = min(old[j-1] + 1, 
+                    min(old[j-1] + 1, 
+                        old[j]+ 1));
             }
         }
+        int *temp = old;
+        old = later;
+        later = temp;
     }
 
-    int val = table[(n * (m+1)) + m];
-    delete [] table;    
+    int val = old[m];
+    delete [] old;
+    delete [] later;    
 
     return PyLong_FromLong(val);
 }
